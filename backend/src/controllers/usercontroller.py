6aaa4345c0af518832068dfbcbed191dcd149b68
swaroop-BTH <swra25@student.bth.se>
2026-05-17 14:40:07 +0200

    created backend-tests.yml and modified usercontroller

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -2,9 +2,12 @@ from src.controllers.controller import Controller
 from src.util.dao import DAO
 
 import re
+
 emailValidator = re.compile(r'.*@.*')
 
+
 class UserController(Controller):
+
     def __init__(self, dao: DAO):
         super().__init__(dao=dao)
 
@@ -30,11 +33,17 @@ class UserController(Controller):
 
         try:
             users = self.dao.find({'email': email})
-            if len(users) == 1:
+
+            if len(users) == 0:
+                return None
+
+            elif len(users) == 1:
                 return users[0]
+
             else:
                 print(f'Error: more than one user found with mail {email}')
                 return users[0]
+
         except Exception as e:
             raise
 
